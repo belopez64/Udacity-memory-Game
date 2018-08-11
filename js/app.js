b0497afa436525e4b9736735fd64e00255470b74
Bennett Lopez <belopez64@gmail.com>
2018-08-11 03:51:23 -0700

    added score functionality

--- js/app.js
+++ js/app.js
@@ -2,7 +2,15 @@
  * Create a list that holds all of your cards and define variables
  */
 let openCards = []
+let moves = 0;
+let score = 3;
 const cards = document.getElementsByClassName('card');
+const deck = document.querySelector('.deck');
+const moveCounter = document.querySelector('.moves');
+const turns = document.querySelector('.text')
+const star1 = document.querySelector('.star1 li');
+const star2 = document.querySelector('.star2 li');
+
 
 /*
  * Display the cards on the page
@@ -11,28 +19,21 @@ const cards = document.getElementsByClassName('card');
  *   - add each card's HTML to the page
  */
 
+ /*
+  *
+  *Main Functions
+  *
+  */
+
  //shuffling functionality
  function shuffleDeck() {
      const unshuffled = [].slice.call(document.querySelectorAll('.deck li'));
-     const shuffledCards = shuffle(unshuffled);
-     console.log('Shuffled cards', shuffledCards);
+     const shuffled = shuffle(unshuffled);
+     for (card of shuffled) {
+         deck.appendChild(card);
+     }
  }
  shuffleDeck();
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
 
 //flip functionality
 for (card of cards) {
@@ -44,25 +45,16 @@ for (card of cards) {
             flippedCards(selection);
             if (openCards.length === 2) {
                 compareCards();
+                incrementMove();
+                removeStar();
             }
+
             cardOpen = true;
+
         }
     });
 }
 
-
-
-//pushes selected card to array "openCards"
-function flippedCards(selection) {
-    openCards.push(selection);
-    console.log(openCards);
-}
-
-//toggles the card between flipped and unflipped
-function flipCard(selection) {
-    selection.classList.toggle('open');
-    selection.classList.toggle('show');
-}
 
 //checks selected card for match
 function compareCards() {
@@ -77,6 +69,64 @@ function compareCards() {
             console.log('No Match');
         }
 }
+
+/*
+ *Functions to be called from main functions
+ */
+
+ // Shuffle function from http://stackoverflow.com/a/2450976
+ function shuffle(array) {
+     var currentIndex = array.length, temporaryValue, randomIndex;
+
+     while (currentIndex !== 0) {
+         randomIndex = Math.floor(Math.random() * currentIndex);
+         currentIndex -= 1;
+         temporaryValue = array[currentIndex];
+         array[currentIndex] = array[randomIndex];
+         array[randomIndex] = temporaryValue;
+     }
+
+     return array;
+ }
+
+//pushes selected card to array "openCards"
+function flippedCards(selection) {
+    openCards.push(selection);
+}
+
+//toggles the card between flipped and unflipped
+function flipCard(selection) {
+    selection.classList.toggle('open');
+    selection.classList.toggle('show');
+}
+
+//adds moves to the counter
+function incrementMove() {
+    moves++;
+    moveCounter.innerHTML = moves;
+    if (moves === 1) {
+        turns.innerHTML = " Move";
+    } else {
+        turns.innerHTML = " Moves";
+    }
+}
+
+//removeStar
+function removeStar() {
+    if (moves === 10) {
+        document.querySelector('.star1').style.display = 'none';
+        score = score - 1;
+    }
+    if (moves === 20) {
+        document.querySelector('.star2').style.display = 'none';
+        score = score - 2;
+    }
+    if (moves === 30) {
+        document.querySelector('.star3').style.display = 'none';
+        score = score - 3;
+    }
+}
+
 
 
 /*timer functionality
